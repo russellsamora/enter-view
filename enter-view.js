95ed1fa5d7508e7bdc3eb02c010b3629d10d3407
russellgoldenberg <russellgoldenberg@gmail.com>
2016-01-24 15:57:52 -0500

    es6 and tweaks

--- enter-view.js
+++ enter-view.js
@@ -12,113 +12,109 @@
     }
 })(function() {
 
-    var lib = function(opts) {
+    const lib = function(opts) {
         
-        var _ticking;
-        var _distanceFromTop;
-		var _elements = [];
+        let raf;
+        let ticking;
+        let distanceFromTop;
+        let elements;
 
-        var init = function() {
-            window.requestAnimationFrame = raf();
-
-            var isValid = hasValidParams();
-
-            if (isValid) {
-				setupDefaultParams();
-				setupElements();
-				setupEvents();
-				updateScroll(); 
-			}
+        const init = function() {
+            hasValidOptions( err => {
+                if (err) { console.error(err); }
+                else {
+                    setupRaf();
+                    setupElements();
+                    setupEvents();
+                    updateScroll();     
+                }
+            });
         };
 
-        var hasValidParams = function() {
+        const hasValidOptions = function(cb) {
+            const required = ['selector', 'trigger'];
 
-            return true;
-        };
-        
-        var setupDefaultParams = function() {
-            
-        };
-
-        var setupElements = function() {
-			// calculate offset from top to trigger at 
-			_distanceFromTop = getOffset();
-			
-			// store all matching elements
-			var query = document.getElementsByClassName(opts.selector);
-			for (var i = 0; i < query.length; i++) {
-				_elements.push(query[i]);
-			}
-		};
-
-        var setupEvents = function() {
-            window.addEventListener('resize', onResize, true);
-            window.addEventListener('scroll', onScroll, true);
-        };
-
-       
-        var onResize = function() {
-        	updateScroll();
-        };
-
-        var onScroll = function() {
-            if (!_ticking) {
-            	_ticking = true;
-                requestAnimationFrame(updateScroll);
+            for (let r in required) {
+                if (!opts[required[r]]) {
+                    cb('missing parameter: ' + required[r]);
+                    return;
+                }
             }
+
+            cb(null);
         };
-
-        var updateScroll = function() {
-            _ticking = false;
-
-            var remove = [];
-			var len = _elements.length;
-
-			for (var i = 0; i < len; i++) {
-				var el = _elements[i];
-				var rect = el.getBoundingClientRect();
-				var top = rect.top;
-				var bottom = rect.bottom;
-
-				if (top < _distanceFromTop) {
-					opts.trigger(el);
-					remove.push(i);
-				}
-			}
-
-			removeElements(remove);
-        };
-
-		var removeElements = function(remove) {
-			var len = remove.length;
-			for (var r = 0; r < len; r++) {
-				_elements.splice(r, 1);
-				r -=1;
-				len = r.length;
-			}
-
-			if (!_elements.length) {
-				window.removeEventListener('scroll', onScroll, true);
-			}
-		};
-
-		var getOffset = function() {
-			var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-			if (opts.offset) {
-				var num = parseInt(opts.offset.replace('%', '')) / 100;
-				var fraction = Math.max(0, Math.min(100, num));
-				return h - fraction * h;
-			}
-			return h;
-		};
-
-        var raf = function() {
-            return window.requestAnimationFrame
+        
+        const setupRaf = function() {
+            raf = window.requestAnimationFrame
                 || window.webkitRequestAnimationFrame
                 || window.mozRequestAnimationFrame
                 || window.msRequestAnimationFrame
                 || function(callback) { return setTimeout(callback, 1000 / 60); };
         };
+
+        const setupElements = function() {
+			// store all matching elements
+            const nodeList = document.querySelectorAll(opts.selector);
+            elements = [];
+            for (let i = 0; i < nodeList.length; i++) {
+                elements.push(nodeList[i]);
+            }
+		};
+
+        const setupEvents = function() {
+            window.addEventListener('resize', onResize, true);
+            window.addEventListener('scroll', onScroll, true);
+
+            // call once on init
+            onResize();
+        };
+
+       
+        const onResize = function() {
+            // calculate offset from top to trigger at 
+            distanceFromTop = getOffset();
+
+        	updateScroll();
+        };
+
+        const onScroll = function() {
+            if (!ticking) {
+            	ticking = true;
+                requestAnimationFrame(updateScroll);
+            }
+        };
+
+        const updateScroll = function() {
+            ticking = false;
+
+			elements = elements.filter( el => {
+
+                const rect = el.getBoundingClientRect();
+                const top = rect.top;
+                const bottom = rect.bottom;
+
+                if (top < distanceFromTop) {
+                    opts.trigger(el);
+                    return false;
+                }
+
+                return true;
+            });
+            
+            if (!elements.length) {
+                window.removeEventListener('scroll', onScroll, true);
+            }
+        };
+
+		const getOffset = function() {
+			const h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+			if (opts.offset) {
+				const num = parseInt(opts.offset.replace('%', '')) / 100;
+				const fraction = Math.max(0, Math.min(100, num));
+				return h - fraction * h;
+			}
+			return h;
+		};
 
         init();
     };
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var babel = require('gulp-babel');
 var browserSync = require('browser-sync').create();
 
 
-gulp.task('default', ['min'], function() {
+gulp.task('default', ['compile'], function() {
 
 	// serve
 	browserSync.init({
@@ -15,12 +16,13 @@ gulp.task('default', ['min'], function() {
 	});
 
 	// watch
-	gulp.watch('enter-view.js', ['min']);
+	gulp.watch('enter-view.js', ['compile']);
 	gulp.watch(['index.html', 'enter-view.min.js'], browserSync.reload);
 });
 
-gulp.task('min', function() {
+gulp.task('compile', function() {
 	return gulp.src('enter-view.js')
+		.pipe(babel({ presets: ['es2015'] }))
 		.pipe(uglify())
 		.pipe(rename('enter-view.min.js'))
 		.pipe(gulp.dest(''));
